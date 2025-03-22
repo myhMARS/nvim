90a1fa3fda5f8b2333741f8973cb65020cce8b4f
myhMARS <1533512157@qq.com>
2025-03-22 16:45:26 +0800

    代码参数补全快捷键更新，以及一些相关插件的版本更新

--- lua/plugins/blinkcmp.lua
+++ lua/plugins/blinkcmp.lua
@@ -32,8 +32,8 @@ return {
 
         ["<Up>"] = { "select_prev", "fallback" },
         ["<Down>"] = { "select_next", "fallback" },
-        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+        ["<C-n>"] = { "snippet_forward", "fallback_to_mappings" },
+        ["<C-p>"] = { "snippet_backward", "fallback_to_mappings" },
 
         ["<C-b>"] = { "scroll_documentation_up", "fallback" },
         ["<C-f>"] = { "scroll_documentation_down", "fallback" },
